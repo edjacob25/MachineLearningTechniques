db0320cc9796145d3d18448fa04e7fb6e5dd1a10
Jacob Rivera <edjacob25@gmail.com>
2019-08-22 13:06:04 -0500

    Added basic code to search

--- Activity1/download_data.py
+++ Activity1/download_data.py
@@ -1,3 +1,15 @@
-import requests
+import configparser
+from elsapy import elsclient
+from elsapy.elssearch import ElsSearch
 
-base_url = "" 
+base_url = ""
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    client = elsclient.ElsClient(config["KEYS"]["elsevier_apikey"])
+
+    aff_srch = ElsSearch('affil(amsterdam)', 'affiliation')
+    aff_srch.execute(client)
+    print("aff_srch has", len(aff_srch.results), "results.")
