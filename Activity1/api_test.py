4257d59327549fa2919850dd7c326d54d1e11177
Jacob Rivera <edjacob25@gmail.com>
2019-09-02 19:08:29 -0500

    Changed data written to the log

--- Activity1/api_test.py
+++ Activity1/api_test.py
@@ -2,12 +2,14 @@ import configparser
 import json
 import requests
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 from itertools import zip_longest
 from time import sleep
 from typing import List
 
 
 @dataclass
+@dataclass_json
 class Institution:
     elsevier_id: str
     name: str
@@ -43,8 +45,10 @@ def institution_search(apikey: str, inst: str, token: str) -> List[Institution]:
     headers = {"X-ELS-APIKey": apikey, "Accept": "application/json"}
     payload = {"query": f"name({inst})"}
     r = requests.get(url, headers=headers, params=payload)
+    print(inst)
     print(r.status_code)
-    print(r.json())
+    print(r.text)
+    #print(r.json())
     options = []
     for result in r.json()["results"]:
         institution = Institution(result["id"], result["name"], result["uri"], result["country"], result["countryCode"])
@@ -52,8 +56,8 @@ def institution_search(apikey: str, inst: str, token: str) -> List[Institution]:
     return options
 
 
-def get_scival_info(apikey: str, elsevier_id: str):
-    print(elsevier_id)
+def get_scival_info(apikey: str, inst: Institution):
+    print(inst.elsevier_id)
     url = "https://api.elsevier.com/analytics/scival/institution/metrics"
     headers = {"X-ELS-APIKey": apikey, "Accept": "application/json"}
 
@@ -62,8 +66,8 @@ def get_scival_info(apikey: str, elsevier_id: str):
                "CollaborationImpact", "FieldWeightedCitationImpact", "PublicationsInTopJournalPercentiles",
                "OutputsInTopCitationPercentiles"]
 
-    with open(f"Data/request_{elsevier_id}.json", "w") as file:
-        params = {"metricTypes": ",".join(metrics), "institutionIds": elsevier_id, "yearRange": "10yrs",
+    with open(f"Data/Scival/{inst.name}_{inst.elsevier_id}.json", "w") as file:
+        params = {"metricTypes": ",".join(metrics), "institutionIds": inst.elsevier_id, "yearRange": "10yrs",
                   "includeSelfCitations": "true", "byYear": "true", "includedDocs": "AllPublicationTypes",
                   "journalImpactType": "CiteScore", "showAsFieldWeighted": "false", "indexType": "hIndex"}
         r = requests.get(url, headers=headers, params=params)
@@ -71,7 +75,9 @@ def get_scival_info(apikey: str, elsevier_id: str):
             print(r.status_code)
             print(r.text)
             result = r.json()["results"][0]["metrics"]
-            file.write(json.dumps(result))
+            entity = inst.to_dict()
+            entity["metrics"] = result
+            file.write(json.dumps(entity))
         sleep(1)
     return result
 
@@ -92,10 +98,14 @@ if __name__ == '__main__':
     with open(filename, "r") as file:
         for line in file:
             uni = line.split(", ")[-1].strip()
+            uni = uni.replace("(", "")
+            uni = uni.replace(")", "")
             try:
                 possible_institutions = institution_search(apikey, uni, token)
-                data = get_scival_info(apikey, possible_institutions[0].elsevier_id)
+                data = get_scival_info(apikey, possible_institutions[0])
             except IndexError:
+                with open("Data/NotFound.txt", "a+") as not_found:
+                    not_found.write(f"{uni}\n")
                 print(f"University {uni} could not be found")
 
     #possible_institutions = institution_search(apikey, "Harvard University", token)
